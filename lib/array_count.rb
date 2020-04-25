c1867289205a83e6874a765c2d327db94255d851
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-25 03:52:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,11 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  stings=0
+array.count do |element|
+  if element.class==String
+    stings +=1
+  end
+end
 end
 
 def count_empty_strings(array)
