66094a2c7c2878474de8fb1903f836c250effda9
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-25 03:54:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -11,7 +11,7 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   empty_string=0
-  array.cout do |element|
+  array.count do |element|
     if element==" " || element==""
       empty_string +=1
     end
